92bf1d92a6d422a7a0a21b50ae4305495f53fc04
josphin89 <josejosphin89@gmail.com>
2024-12-05 16:58:44 +0530

    javascriptarrayinput

---
+++ resume_builder.js
@@ -0,0 +1,28 @@
+let resume = {
+    personal_details:{},
+    additional:{},
+    skills:[],
+    hobbies:[],
+};
+
+function addValue(e,key,p_key){
+    if(p_key){
+        resume[p_key][key]=e.value;
+    }
+    else{
+        resume[key]= e.value;
+    }
+
+  display_output()
+}
+
+function addArrayValue(key){
+    let value=document.getElementById(key).value;
+    resume[key].push(value)
+    document.getElementById(key).value="";
+    display_output()
+}
+
+function display_output(){
+  document.getElementById('output').innerHTML= JSON.stringify(resume)
+}
